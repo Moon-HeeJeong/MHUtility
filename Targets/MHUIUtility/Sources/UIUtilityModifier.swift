9b8f2a524044081f36c651e6634e6bd894bb3cb3
정문희 <jg.moonhee@littlefox.com>
2023-08-28 10:00:58 +0900

    MHUIUtility 수정중...

--- Targets/MHUIUtility/Sources/UIUtilityModifier.swift
+++ Targets/MHUIUtility/Sources/UIUtilityModifier.swift
@@ -32,18 +32,14 @@ struct UIUtilityModifier: ViewModifier{
             content.alert(isPresented: isShow) {
                 switch info?.type{
                 case .oneBtn_confirm(let actionTitle, let action):
-                    let actionBtn = Alert.Button.default(Text(actionTitle ?? "확인"), action: action)
+                    let title = actionTitle ?? "확인"
                     
-                    return Alert(title:  Text(info?.title ?? ""), message: Text(info?.message ?? ""), dismissButton: actionBtn)
-                    
-                    
-//                    if let action = action{
-//                        let actionBtn = Alert.Button.default(Text(actionTitle ?? "확인"), action: action)
-//
-//                        return Alert(title:  Text(info?.title ?? ""), message: Text(info?.message ?? ""), dismissButton: actionBtn)
-//                    }else{
-//                        return Alert(title: Text(info?.title ?? ""), message: Text(info?.message ?? ""), dismissButton: Alert.Button.default(Text(actionTitle ?? "확인")))
-//                    }
+                    if let action = action{
+                        let actionBtn = Alert.Button.default(Text(title), action: action)
+                        return Alert(title:  Text(info?.title ?? ""), message: Text(info?.message ?? ""), dismissButton: actionBtn)
+                    }else{
+                        return Alert(title: Text(info?.title ?? ""), message: Text(info?.message ?? ""), dismissButton: Alert.Button.default(Text(title)))
+                    }
 
                 case .twoBtn(let actionTitle, let action):
                     let actionBtn = Alert.Button.default(Text(actionTitle ?? "확인"), action: action)
