78627cc685418b1f59dc1e447b3dd8617acff663
정문희 <jg.moonhee@littlefox.com>
2023-09-15 13:55:10 +0900

    MHUIUtility - MHAlert 수정

--- Targets/MHUIUtility/Sources/UIUtilityInfo.swift
+++ Targets/MHUIUtility/Sources/UIUtilityInfo.swift
@@ -73,7 +73,7 @@ public enum UIUtility_E: Identifiable, Equatable{
 
 public enum AlertType{
     case oneBtn_confirm(actionTitle: String? = nil, action: (()->())? = nil)
-    case twoBtn(actionTitle:String?, action: ()->(), noneActionTitle: String?)
+    case twoBtn(actionTitle:String? = nil, action: ()->(), noneActionTitle: String? = nil)
 }
 
 public struct AlertInfo: Identifiable{
