0939513132dd88a565eeaa8e7c30952dcca5e636
정문희 <jg.moonhee@littlefox.com>
2024-01-12 15:10:40 +0900

    MHAPI 예외처리 수정

---
+++ Targets/MHCoreData/CoreDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataManager.swift
+//  MHAPI
+//
+//  Created by LittleFoxiOSDeveloper on 1/8/24.
+//
+
+import Foundation
---
+++ Targets/MHUtilityTestAppSwiftUI/Sources/TestViews/CoreDataTestView.swift
@@ -0,0 +1,18 @@
+//
+//  CoreDataTestView.swift
+//  MHUtilityTestAppSwiftUI
+//
+//  Created by LittleFoxiOSDeveloper on 1/9/24.
+//
+
+import SwiftUI
+
+struct CoreDataTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoreDataTestView()
+}
