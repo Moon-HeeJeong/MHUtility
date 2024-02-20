57993896c725d59da9c50d5df7b942a3a331e498
박찬도 <cantao@kakao.com>
2024-02-20 14:06:12 +0900

    2/20

--- Targets/MHAPI/Sources/APIResponse.swift
+++ Targets/MHAPI/Sources/APIResponse.swift
@@ -14,9 +14,9 @@ public protocol Response_P: Model_T{
     associatedtype Model: Model_T
     
     var responseType: Response_E {get set}
-    var data: Model? {get set}
+    var data: Model {get set}
     
-    init(responseType: Response_E, data: Model?)
+    init(responseType: Response_E, data: Model)
 }
 
 public enum Response_E{
@@ -56,7 +56,7 @@ public enum Response_E{
 public struct Response<Model: MHDataType_P>: Response_P{
     
     public var responseType: Response_E
-    public var data: Model?
+    public var data: Model
     
     public var accessToken: String?{
         get{
@@ -76,7 +76,7 @@ public struct Response<Model: MHDataType_P>: Response_P{
         case access_token
     }
     
-    public init(responseType: Response_E, data: Model?) {
+    public init(responseType: Response_E, data: Model) {
         self.responseType = responseType
         self.data = data
     }
@@ -88,18 +88,18 @@ public struct Response<Model: MHDataType_P>: Response_P{
         let status = try? container.decode(Int.self, forKey: .status)
         let message = try? container.decode(String.self, forKey: .message)
         
-        self.data = try? container.decode(Model.self, forKey: .data)
+        self.data = try container.decode(Model.self, forKey: .data)
         
         if status == 200{
 //            self.responseType = .ok(message: message)
             self.responseType = .ok(code: 200, message: message)
         }else{
-            if let _ = self.data{
+//            if let _ = self.data{
 //                self.responseType = .ok(message: message)
-                self.responseType = .ok(code: status ?? 200, message: message)
-            }else{
+//                self.responseType = .ok(code: status ?? 200, message: message)
+//            }else{
                 self.responseType = .error(code: status ?? -1, message: message)
-            }
+//            }
         }
         
         self.accessToken = try? container.decode(String.self, forKey: .access_token)
